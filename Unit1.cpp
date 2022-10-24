96f1d6496ecceacf505e8296ad2000b52d50bcb3
TohyNyAina <tohiniainarazafi@gmail.com>
2022-10-24 10:51:45 +0200

    PGCD PPCM

---
+++ Unit1.cpp
@@ -0,0 +1,40 @@
+#include<math.h>
+#include<stdio.h>
+#include<conio.h>
+int a,b,x,t,m;
+void main()
+        {
+        printf("Entrez le premier entier positif:\t");
+        scanf("%d",&a);
+        printf("\nEntrez le deuxieme entier positif:\t");
+        scanf("%d",&b);
+        if (b>a)
+        {
+                x=b;
+                b=a;
+                a=x;
+        }
+
+        if (a%b==0)
+        printf("\nLe PPCM est :\t%d",a);
+        else
+        {
+                t=(a*b);
+                while(b != 0)
+                {
+                        x = a%b;
+                        a = b;
+                        b = x;
+                }
+        m=t/a;
+        printf("\nLe PPCM est :\t%d ",m);
+        }
+        while(b != 0)
+        {
+        x = a%b;
+        a = b;
+        b = x;
+        }
+        printf("\nLe PGCD est :\t%d", a);
+getch();
+}
